a1ed0a37e7531031c6738a28dc246d96ac24bde9
evgeniia-korolova <jkod@ukr.net>
2023-08-08 16:28:27 +0300

    feat : add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  
+  const pairs = new Map(bracketsConfig); 
+  const stack = []; 
+  for (const brack of str) {
+    
+    if (pairs.get(stack.at(-1)) == brack) {
+      
+      stack.pop(); 
+    } else {
+      
+      stack.push(brack); 
+    }
+  }
+  return stack.length == 0; 
+};
